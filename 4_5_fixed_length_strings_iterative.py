650bb0bc894e16a34f39fca21ae024d686707c8a
Vladimir Vladimirov <smartkiwi@gmail.com>
2019-10-17 12:21:46 -0600

    4.7.3 generation using arithmetic sum (first implementation)

--- 4_5_fixed_length_strings_iterative.py
+++ 4_5_fixed_length_strings_iterative.py
@@ -43,16 +43,69 @@ def strings_2_4_7_2(n):
     int_s = int(s, 2)
     end_result = '0b' + '1' * n
     int_end_result = int(end_result, 2)
-    yield format_bin(int_s, n)
+    yield _format_bin(int_s, n)
     while int_s < int_end_result:
         int_s += 1
-        yield format_bin(int_s, n)
+        yield _format_bin(int_s, n)
 
 
-def format_bin(val, n):
+def _format_bin(val, n):
     bin_str = bin(val)
     bin_str = bin_str.replace('0b', '')
     return bin_str.zfill(n)
+
+
+def strings_2_4_7_3(A, n):
+    """Exercise 4.7.3 (Medium): The main operation in this algorithm is very similar to a particular arithmetic operation.
+    What operation is that? Can you re-implement the algorithm using arithmetic operations? In particular, for the
+    binary alphabet given in Exercise 4.7.2, can you implement this algorithm very efficiently using bitwise operations and basic arithmetic?"""
+    index_of = {x: i for i, x in enumerate(A)}
+    s = [A[0]] * n
+    final_array = [A[-1]] * n
+    int_s = base_to_int(
+        char_to_base_num(s, index_of), len(A)
+    )
+    int_final = base_to_int(char_to_base_num(final_array, index_of), len(A))
+    yield ''.join(s)
+    while int_s < int_final:
+        int_s += 1
+        yield ''.join(base_num_to_char(int_to_base(int_s, len(A)), A)).ljust(len(A), A[0])
+
+
+def char_to_base_num(array_of_chars, index_of):
+    array_of_digits = []
+    for el in array_of_chars:
+        array_of_digits.append(index_of[el])
+    return array_of_digits
+
+
+def base_num_to_char(array_of_digits, A):
+    array_of_chars = []
+    for el in array_of_digits:
+        array_of_chars.append(A[el])
+    return array_of_chars
+
+
+def int_to_base(num, base):
+    """converts int into number in base system"""
+    max_num = int(math.log(num, base))
+    array_of_digits = []
+    remainder = num
+    for pow in range(max_num, -1, -1):
+        full_digit = math.pow(base, pow)
+        pos_value = int(remainder // full_digit)
+        remainder = remainder % math.pow(base, pow)
+        array_of_digits.append(pos_value)
+    return array_of_digits
+
+
+def base_to_int(array_of_digits, base):
+    """converts number in base system to int"""
+    int_value = 0
+    for ind, el in enumerate(array_of_digits):
+        pow = len(array_of_digits) - ind - 1
+        int_value += el * math.pow(base, pow)
+    return int_value
 
 
 def t_n(k, n):
@@ -73,3 +126,6 @@ if __name__ == "__main__":
     print "'%s'" % "','".join(strings_2(['0', '1'], n=3))
     print strings_2_4_7_2.__doc__
     print list(strings_2_4_7_2(n=3))
+
+    print strings_2_4_7_3.__doc__
+    print list(strings_2_4_7_3(['0', '1', '2'], n=3))
